f18835662f4b8a7c03e6ce1c959ca8a656a937fc
kuzubaka <kuzubaka.out0727@gmail.com>
2026-09-10 20:54:00 +0900

    Neovim設定: 整理後-未完成2

--- lua/behavior.lua
+++ lua/behavior.lua
@@ -1,19 +1,27 @@
 local vo = vim.o
 
 vo.clipboard = "unnamedplus"
+vo.foldlevel = 99
+vo.foldlevelstart = 99
+vo.foldenable = true
+v.o.report = 999999999
+vim.bo.omnifunc = "syntaxcomplete#Complete"
+vim.opt.fixeol = false
+vim.opt.conceallevel = 0
+vim.opt.fileformats = { "unix", "dos" }
 
 -- Indent Format
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		local v = vim.fn.winsaveview();
-		pcall(vim.cmd, "silent undojoin");
-		vim.cmd("keepjumps normal! gg=G");
-		vim.fn.winrestview(v);
-	end
+ callback = function()
+  local v = vim.fn.winsaveview();
+  pcall(vim.cmd, "silent undojoin");
+  vim.cmd("keepjumps normal! gg=G");
+  vim.fn.winrestview(v);
+ end
 })
 
 -- Comment
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function() vim.opt.formatoptions:remove({ "c", "r", "o" }) end,
+ pattern = "*",
+ callback = function() vim.opt.formatoptions:remove({ "c", "r", "o" }) end,
 })

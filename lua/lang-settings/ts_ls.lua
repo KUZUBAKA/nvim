1edd51b1eb34be38b3de0570a57baeecba90fd11
kuzubaka <kuzubaka.out0727@gmail.com>
2026-09-15 00:46:06 +0900

    Neovim設定: Minecraft Bedrock アドオン開発環境構築11

--- lua/lang-settings/ts_ls.lua
+++ lua/lang-settings/ts_ls.lua
@@ -1,9 +1,12 @@
 local dir = vim.fn.stdpath("data") .. "/bedrock-scripts"
-local cmd = { "npm", "install", "--prefix", dir, "--no-package-lock", "--no-fund", "typescript@6", "typescript-language-server" }
+local cmd = { "npm", "install", "--prefix", dir, "--no-package-lock", "no-audit" ,"--no-fund", "typescript@6", "typescript-language-server" }
 
 if not vim.uv.fs_stat(dir) then
  vim.fn.system(cmd)
 end
 
 return function()
+ vim.lsp.config("ts_ls", {
+  cmd = { dir .. "/node_module/.bin/typescript-language-server", " --studio" },
+ })
 end

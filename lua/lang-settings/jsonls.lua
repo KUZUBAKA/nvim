f3b22a2d1d0e35977d8ac20aeb5183c0a7d752d4
kuzubaka <kuzubaka.out0727@gmail.com>
2026-09-14 16:27:58 +0900

    Neovim設定: Minecraft Bedrock アドオン開発環境構築2

--- lua/lang-settings/jsonls.lua
+++ lua/lang-settings/jsonls.lua
@@ -1,5 +1,10 @@
 return function()
  vim.lsp.config("jsonls", {
-  settings = { json = { schemas = {} } },
+  settings = { json = { schemas = {
+   {
+    fileMatch = { "manifest.json" },
+    url = "https://raw.githubusercontent.com/Blockception/Minecraft-bedrock-json-schemas/main/general/manifest.json",
+   },
+  } } },
  })
 end

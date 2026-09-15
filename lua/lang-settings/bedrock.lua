6a616a1abf5ad2329ebc1ada6d5847fa1730b6bd
kuzubaka <kuzubaka.out0727@gmail.com>
2026-09-15 16:20:36 +0900

    Neovim設定: Minecraft Bedrock アドオン開発環境構築18

--- lua/lang-settings/bedrock.lua
+++ lua/lang-settings/bedrock.lua
@@ -4,6 +4,7 @@ local url = "https://github.com/Blockception/minecraft-bedrock-language-server/r
 if not vim.uv.fs_stat(dir) then
  vim.fn.system({ "curl", "-sL", "--create-dirs", "-o", dir .. "/vscode-extension.vsix", url })
  vim.fn.system({ "unzip", "-oq", dir .. "/vscode-extension.vsix", "extension/lsp/server.js", "extension/minecraft-*/syntax.tmLanguage.json", "-d", dir })
+ vim.fn.delete(dir .. "/vscode-extension.vsix")
 end
 
 return function()

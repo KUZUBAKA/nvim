536809c5c575555db257052619e7240c9f48bf65
kuzubaka <kuzubaka.out0727@gmail.com>
2026-09-15 15:11:14 +0900

    Neovim設定: Minecraft Bedrock アドオン開発環境構築17

--- lua/lang-settings/bedrock.lua
+++ lua/lang-settings/bedrock.lua
@@ -1,2 +1,5 @@
 local dir = vim.fn.stdpath("data") .. "/bedrock-lsp"
 local url = "https://github.com/Blockception/minecraft-bedrock-language-server/releases/latest/download/vscode-extension.vsix"
+
+if not vim.uv.fs_stat(dir) then
+end
